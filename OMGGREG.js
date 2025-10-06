80051f8f9c4ee7be46e731f2232bbf0a3d2a4f3f
DYenn00 <55598370+DYenn00@users.noreply.github.com>
2025-10-05 23:09:52 -0400

    Create OMGGREG.js

---
+++ OMGGREG.js
@@ -0,0 +1,12 @@
+// "OMGGREG"
+// song @by EVZ
+// script @by DYenn
+
+
+$:note("c2 <eb2 <g2 g1>>".fast(8))
+.sound("z_pink pulse:<32 16 8 4>")
+._scope()
+$ :note("{f a c e}%8").s("triangle").tremsync(4).tremolophase("<0 .25 .66>")
+$ :sound("bd*2,<white pink brown>*8")
+.decay(.04).sustain(0)._scope()
+$ :sound("bd c2 g2 bd").bank("RolandTR808")
